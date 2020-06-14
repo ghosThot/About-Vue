0d866acda175d64ac119f351130dbc932da4a361
ghosThot <1018513521@qq.com>
2020-06-14 23:38:01 +0800

    nuxt init

---
+++ pages/detail.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    detail parent page
+
+     <!-- 相当于router-link -->
+    <nuxt-child></nuxt-child>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
